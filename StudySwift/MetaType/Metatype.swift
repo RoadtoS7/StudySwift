9e613e1f97d95a9aa5be55516e41986a75693dee
nylah-j <nylah.j@kakaoent.com>
2022-12-30 23:51:23 +0900

    프로토콜의 메타타입 = .Protocol, Any는 모든 타입이 comfort하는 타입

--- StudySwift/MetaType/Metatype.swift
+++ StudySwift/MetaType/Metatype.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+
+protocol Devicable {}
+class MyDevice: Devicable {}
+
 class MetaTypeStudy {
     struct Medium {
         static var author: String = "RoadotS7"
@@ -28,4 +32,16 @@ class MetaTypeStudy {
         let staticType: Medium.Type = Medium.self
         print("staticType: \(staticType)") // Medium
     }
+    
+    /// 프로토콜.Type = 모든 프로토콜 구현체의 메타타입
+    /// 프로토콜.Protocol = 프로토콜의 메타타입
+    /// 프로토콜의 메타타입을 이용해서 주로 하는 일? 타입 일치여부 체크
+    /// 예외: Any = Any는 모든 타입이 comfort하는 타입이다.
+    static func protocolMetattype() {
+//        let nonProtocolMetatype: Devicable.Type = Devicable.self // 컴파일 에러
+        let metatype: Devicable.Type = MyDevice.self
+        let protocolMetatype: Devicable.Protocol = Devicable.self
+        let anyType: Any.Type = Any.self
+    }
 }
+
