4ff8fcc3fd87b9a8c7d2a1417f60675f8762a5e5
RoadtoS7 <kshvv975@gmail.com>
2024-09-03 23:10:00 +0900

    Add Swift6 > RangeSet 등장

---
+++ StudySwift/Swift6/RangeSet.swift
@@ -0,0 +1,34 @@
+//
+//  RangeSet.swift
+//  StudySwift
+//
+//  Created by 김수현 on 9/4/24.
+//
+// RangeSet이라는 새로운 타입이 등장
+
+import Foundation
+
+//final class TestRangeSet {
+//    struct ExamResult {
+//        var student: String
+//        var score: Int
+//    }
+//
+//    let results = [
+//        ExamResult(student: "Eric Effiong", score: 95),
+//        ExamResult(student: "Maeve Wiley", score: 70),
+//        ExamResult(student: "Otis Milburn", score: 100)
+//    ]
+//    
+//    func test() {
+//        let topResultIndices = results.indices { student in
+//            student.score >= 85
+//        }
+//        
+//        for result in results[topResultIndices] {
+//            print("\(result.student) scored \(result.score)%")
+//        }
+//    }
+//}
+
+
