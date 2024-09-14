c4f1b8761b6b35fe3a229556bb12ec814599c4d2
RoadtoS7 <kshvv975@gmail.com>
2024-09-14 12:44:54 +0900

    Count(where:) 주석 처리, XCode 17에서 사용가능

--- StudySwift/Swift6/CountWhere.swift
+++ StudySwift/Swift6/CountWhere.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-final class CountWhere {
-    func test() {
-        let score = [100, 80, 85]
-        let passCount = score.count { $0 > 85 }
-    }
-}
+// XCode 17에서 사용가능 
+//final class CountWhere {
+//    func test() {
+//        let score = [100, 80, 85]
+//        let passCount = score.count { $0 > 85 }
+//    }
+//}
