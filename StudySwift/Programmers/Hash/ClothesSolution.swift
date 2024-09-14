0ab61cc70297c021438031960fcce857690f8ff0
RoadtoS7 <kshvv975@gmail.com>
2024-09-15 00:12:00 +0900

    Add 프로그래머스 > 해시 > 의상

---
+++ StudySwift/Programmers/Hash/ClothesSolution.swift
@@ -0,0 +1,41 @@
+//
+//  ClothesSolution.swift
+//  StudySwift
+//
+//  Created by nylah.j on 9/14/24.
+//
+
+import Foundation
+
+final class ClothesSolution {
+    func solution(_ clothes:[[String]]) -> Int {
+        var closet: [String: [String]] = [:]
+        
+        for cloth in clothes {
+            let (name, type) = (cloth[0], cloth[1])
+            
+            if let names = closet[type] {
+                closet[type] = names + [name]
+            } else {
+                closet[type] =  [name]
+            }
+        }
+        
+        // 1개만 고르는 경우, 2개만 고르는 경우
+        let setCount = closet.reduce(into: 1) { partialResult, closet in
+            partialResult *= (closet.value.count + 1)
+        }
+        return setCount - 1
+    }
+    
+    static func test() {
+        [
+            [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]],
+            [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]],
+            [["crow_mask", "face"]]
+        ].forEach { clothes in
+            print(ClothesSolution().solution(clothes))
+        }
+        
+    }
+}
