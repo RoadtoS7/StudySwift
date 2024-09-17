647056828c37e591fba1ca21ce8db3f334962c85
RoadtoS7 <kshvv975@gmail.com>
2024-09-17 22:10:21 +0900

    Add 프로그래머스 > BFS/DFS > 단어변환 - 5

--- StudySwift/Programmers/BFS_DFS/ConvertWords.swift
+++ StudySwift/Programmers/BFS_DFS/ConvertWords.swift
@@ -46,4 +46,62 @@ final class ConvertWords {
             }
         }
     }
+    
+    func solution2(_ begin:String, _ target:String, _ words:[String]) -> Int {
+        // 몇단개를 거칠 수 있는가?
+        // 알파벳이 한개가 차이나는가를 어떻게 알아..?
+        guard words.contains(target) else {
+            return 0
+        }
+        
+        let words = words + [begin]
+        var network: [String: [String]] = [:]
+        
+        for i in 0..<words.count {
+            for j in 0..<words.count {
+                if words[i] != words[j], diffCount(words[i], words[j]) == 1 {
+                    network[words[i], default: []].append(words[j])
+                    network[words[j], default: []].append(words[i])
+                }
+            }
+        }
+        
+        var visited: [String:Bool] = words.reduce(into: [String:Bool]()) { partialResult, word in
+            partialResult[word] = false
+        }
+        
+        var minCount: Int = .max
+        var needToVisit = [(begin, 0, visited)]
+        while needToVisit.isEmpty == false {
+            var (node, count, visited): (String, Int, [String:Bool]) = needToVisit.removeFirst()
+            if node == target {
+                minCount = min(minCount, count)
+                continue
+            }
+            
+            for willVisit in network[node, default: []] {
+                if visited[willVisit] == false {
+                    visited[willVisit] = true
+                    needToVisit.append((willVisit, count + 1, visited))
+                }
+            }
+        }
+        
+        return minCount == .max ? 0 : minCount
+    }
+    
+    private func diffCount(_ word1: String, _ word2: String) -> Int {
+        let w1 = Array(word1)
+        let w2 = Array(word2)
+        return zip(w1, w2).filter { $0 != $1 }.count
+    }
+    
+    static func test() {
+        [
+            ("hit",    "cog",    ["hot", "dot", "dog", "lot", "log", "cog"]),
+            ("hit",   "cog",    ["hot", "dot", "dog", "lot", "log"])
+        ].forEach {
+            print(ConvertWords().solution2($0.0, $0.1, $0.2))
+        }
+    }
 }
