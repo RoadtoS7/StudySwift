a76a5631314985eb14dcd269b6cbd5b9d1691fb1
RoadtoS7 <kshvv975@gmail.com>
2024-11-09 17:37:33 +0900

    FlatMap 을 비동기 작업을 순차처리하기 위해 사용하는 예시 코드

--- StudySwift/Combine/FlatMap.swift
+++ StudySwift/Combine/FlatMap.swift
@@ -53,3 +53,79 @@ class PublisherTest {
         personC.name.send("Suhyeon") // personC는 flatMap의 buffer에 저장되지 않았기 때문에 print되지 않는다.
     }
 }
+
+class SerialPublisherTest {
+    func test() {
+        let cancellable =  performSequentialTasks()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("모든 작업이 성공적으로 완료되었습니다.")
+                case .failure(let error):
+                    print("작업 중 오류 발생: \(error.localizedDescription)")
+                }
+                
+            }, receiveValue: {
+                print("데이터가 성공적으로 저장되었습니다.")
+                
+            })
+    }
+    
+    func performSequentialTasks() -> AnyPublisher<Void, Error> {
+        fetchData().flatMap { data -> AnyPublisher<String, Error> in
+            if data == "특정 조건" {
+                return Empty(completeImmediately: true).eraseToAnyPublisher()
+            } else {
+                return self.processData(data).eraseToAnyPublisher()
+            }
+        }.flatMap({ processedData -> AnyPublisher<Void, Error> in
+            if processedData == "다른 조건" {
+                return Fail(error: NSError(domain: "Custom Error", code: -1, userInfo: nil)).eraseToAnyPublisher()
+            } else {
+                return self.saveData(processedData).eraseToAnyPublisher()
+            }
+        })
+        .eraseToAnyPublisher()
+            
+    }
+    
+    /// 네트워크 요청을 시뮬레이션하는 비동기 작업
+    func fetchData() -> AnyPublisher<String, Error> {
+        Future<String, Error> { promise in
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: .init(block: {
+                let success = Bool.random()
+                if success {
+                    promise(.success("데이터 수신 성공"))
+                } else {
+                    promise(.failure(NSError(domain: "NetworkError", code: -1, userInfo: nil)))
+                }
+            }))
+        }.eraseToAnyPublisher()
+    }
+    
+    func processData(_ data: String) -> AnyPublisher<String, Error> {
+        Future<String, Error> { promise in
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: .init(block: {
+                if data.contains("성공") {
+                    promise(.success("데이터 처리 완료"))
+                } else {
+                    promise(.failure(NSError(domain: "ProcessingError", code: -1, userInfo: nil)))
+                }
+            }))
+        }.eraseToAnyPublisher()
+    }
+    
+    func saveData(_ data: String) -> AnyPublisher<Void, Error> {
+        Future<Void, Error> { promise in
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: .init(block: {
+                let success = Bool.random()
+                if success {
+                    promise(.success(()))
+                } else {
+                    promise(.failure(NSError(domain: "SaveError", code: -1, userInfo: nil)))
+                }
+            }))
+        }.eraseToAnyPublisher()
+    }
+    
+}
